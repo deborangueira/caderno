67bebdc59a3b0e67d7bd992b567c8df156ed1a45
Débora Nogueira <debora.nogueira@sou.inteli.edu.br>
2025-07-25 12:57:23 -0300

    1010

--- exercicios/1010/script1010_avancado.js
+++ exercicios/1010/script1010_avancado.js
@@ -2,21 +2,18 @@ var input =  require('fs').readFileSync('exercicios/stdin','utf8');
 
 var lines = input.split('\n'); // no beecrowd essa linha deve ser trocada para: var lines = input.trim().split(/\r?\n/); 
 
-var product = {} // objeto para armazenar dados dos produtos identificados por com chaves dinâmicas
 var total = 0
 
 for(let i=0; i <lines.length; i++) { // acesso uma linha por vez do array lines
+
     let parts = lines[i].split(' ');
+
     let code = parts[0];
     let quantity = Number(parts[1]);
     let price = Number(parts[2]);
-    product[code] = {quantidade: quantity, valorUnitario: price}; // guardo esses dados dentro do objeto produto e separo-os com base no código de cada produto.
-}
 
-for (let code in product) { // loop para percorrer as chaves de um objeto
-    let quantity = product[code].quantidade;
-    let price = product[code].valorUnitario;
-    total += (quantity*price);
+    total += (quantity*price)
+
 }
 
 console.log(`VALOR A PAGAR: R$ ${total.toFixed(2)}`);
