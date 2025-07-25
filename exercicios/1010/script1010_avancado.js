680353f4100c762ce1e9ecb3a3dc512421490335
Débora Nogueira <debora.nogueira@sou.inteli.edu.br>
2025-07-25 13:09:53 -0300

    1010

--- exercicios/1010/script1010_avancado.js
+++ exercicios/1010/script1010_avancado.js
@@ -1,19 +1,22 @@
-var input =  require('fs').readFileSync('exercicios/stdin','utf8');
+var input = require("fs").readFileSync("exercicios/stdin", "utf8");
 
-var lines = input.split('\n'); // no beecrowd essa linha deve ser trocada para: var lines = input.trim().split(/\r?\n/); 
+var lines = input.split("\n"); // no beecrowd essa linha deve ser trocada para: var lines = input.trim().split(/\r?\n/);
 
-var total = 0
+var product = {}
+var total = 0;
 
-for(let i=0; i <lines.length; i++) { // acesso uma linha por vez do array lines
+for (let i = 0; i < lines.length; i++) {
+  // acesso uma linha por vez do array lines
 
-    let parts = lines[i].split(' ');
+  let parts = lines[i].split(" ");
 
-    let code = parts[0];
-    let quantity = Number(parts[1]);
-    let price = Number(parts[2]);
+  let code = parts[0];
+  let quantity = Number(parts[1]);
+  let price = Number(parts[2]);
 
-    total += (quantity*price)
+  total += quantity * price;
 
+  product[code] = { quantidade: quantity, valorUnitario: price }; // guardo esses dados dentro do objeto produto e separo-os com base no código de cada produto.
 }
 
 console.log(`VALOR A PAGAR: R$ ${total.toFixed(2)}`);
