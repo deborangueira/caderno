34e48e79313a4bcf62cd1fe71ed8db7984976032
Débora Nogueira <debora.nogueira@sou.inteli.edu.br>
2025-08-13 14:42:31 -0300

    notes 1480

---
+++ exercicios/leetcode/1480/1672/script1672.js
@@ -0,0 +1,17 @@
+accounts = [
+  [1, 5],
+  [7, 3],
+  [3, 5],
+];
+
+var wealth = 0;
+
+for (i = 0; i < accounts.length; i++) {
+  let client = accounts[i];
+
+  for (i = 0; i < client.length; i++) {
+    wealth += client[i];
+  }
+
+  console.log(client);
+}
