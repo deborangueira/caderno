c4924d998b0be537c91998f8a5138fa5011ccc4e
Débora Nogueira <debora.nogueira@sou.inteli.edu.br>
2025-08-13 10:43:06 -0300

    notes 1480

--- exercicios/leetcode/1480/1480/script1480.js
+++ exercicios/leetcode/1480/1480/script1480.js
@@ -1,13 +1,10 @@
 let nums = [1,2,3,4]
 
-var runningSum = function(nums) {
-
 let result = [nums[0]];
 
 for (i = 1; i < nums.length; i++) {
     let soma = nums[i] + result[i-1]
     result.push(soma);
 }
-return result;
-};
 
+console.log(result)
--- exercicios/script_MODEL/iniciante.js
+++
@@ -1,2 +0,0 @@
-var input =  require('fs').readFileSync('exercicios/stdin','utf8');
-var lines = input.split('\n');
