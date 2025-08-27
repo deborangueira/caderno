d34b4f051b1b62ca5558d1551c5395af9133098c
Débora Nogueira <debora.nogueira@sou.inteli.edu.br>
2025-08-27 09:15:12 -0300

    01 two sum

---
+++ exercicios/leetcode/1_two_sum/notes1.md
@@ -0,0 +1,6 @@
+### Roadmap
+
+1. pega o 1º índice e soma com todos os outros a frente dele até o final da lista.
+2. se a soma entre o termo e o próximo der igual ao target: 
+    - retorna o índice dos valores
+    - para de tentar
--- exercicios/leetcode/1_two_sum/script.js
+++ exercicios/leetcode/1_two_sum/script.js
@@ -1,8 +1,20 @@
 
-nums = [2,7,11,15]
+// nums = [3,2,4]
 
-target = 9
+// target = 9
 
 var twoSum = function(nums, target) {
     
+    for (let i=0; i<nums.length;i++){    
+        term = nums[i]
+        index = i
+
+        for (let i = (index+1); i <nums.length; i++){
+            if(term + nums[i] == target){
+                return [index,i]
+                break;
+            }
+        }
+    }
 }
+
