ebba9c7be358eb90621c33a346b3f9e4f8276e40
Débora Nogueira <deboranogueira@192.168.0.12>
2026-05-03 20:07:49 -0300

    docs: encapsulamento

--- conteudo/POO/script-exercicio.py
+++ conteudo/POO/script-exercicio.py
@@ -30,3 +30,24 @@ carro2.acelerar()
 carro2.frear()
 carro2.buzinar()
 carro2.get_info()
+
+# ENCAPSULAMENTO -> quando a classe vira responsável por PROTEGER seus próprios dados. Isso é feito com o uso de atributos privados (__preco) e o uso de getters e setters. Sem isso o código pode apresentar inconsistência e comportamento imprevisível pois diferentes partes do codigo poderiam alterar o valor de um atributo de forma independente e sem controle. A classe nesse sentido se torna a "fonte de verdade" para os dados. Isso contribui para o conceito de Modelagem de Domínio com POO que foca em representar regras de negócio (fluxos, dados, comportamentos) em objetos de software, não apenas tabelas de banco de dados. Ela utiliza entidades, agregados e value objects para encapsular regras próximo aos dados, evitando modelos anêmicos e promovendo a coesão no sistema.
+
+class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.__preco = preco # __preco é um atributo privado (não pode ser acessado fora da classe)
+
+    def get_preco(self): # Getter, método para acessar o atributo privado
+        return self.__preco
+
+    def set_preco(self, preco): # Setter, método para alterar o atributo privado
+        if preco > 0:
+            self.__preco = preco
+        else:
+            print("O preço deve ser maior que 0")
+
+produto1 = Produto("Camiseta", 100)
+print(produto1.get_preco())
+produto1.set_preco(150)
+print(produto1.get_preco())
